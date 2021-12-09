e8d068aa2226e2cbd266f4cb27790da1c84bc424
M.A. Jigsaw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-09 08:52:39 +0200

    If You Compile On Windows

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -159,10 +159,18 @@ class FreeplayState extends MusicBeatState
 		var textBG:FlxSprite = new FlxSprite(0, FlxG.height - 26).makeGraphic(FlxG.width, 26, 0xFF000000);
 		textBG.alpha = 0.6;
 		add(textBG);
-		#if PRELOAD_ALL
-		var leText:String = "Press X to listen to this Song / Press Y to Reset your Score and Accuracy.";
+		#if windows
+			#if PRELOAD_ALL
+			var leText:String = "Press SPACE to listen to this Song / Press RESET to Reset your Score and Accuracy.";
+			#else
+			var leText:String = "Press RESET to Reset your Score and Accuracy.";
+			#end
 		#else
-		var leText:String = "Press Y to Reset your Score and Accuracy.";
+                        #if PRELOAD_ALL
+			var leText:String = "Press X to listen to this Song / Press Y to Reset your Score and Accuracy.";
+			#else
+			var leText:String = "Press Y to Reset your Score and Accuracy.";
+			#end			
 		#end
 		var text:FlxText = new FlxText(textBG.x, textBG.y + 4, FlxG.width, leText, 18);
 		text.setFormat(Paths.font("vcr.ttf"), 18, FlxColor.WHITE, RIGHT);
