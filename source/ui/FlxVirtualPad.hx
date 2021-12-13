3176fe544c0027183a171ce065aea11ed12cba66
M.A. Jigsaw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-13 12:19:04 +0200

    New position for 7 key button

--- source/ui/FlxVirtualPad.hx
+++ source/ui/FlxVirtualPad.hx
@@ -85,7 +85,7 @@ class FlxVirtualPad extends FlxSpriteGroup
 			case A_B_C:
 				actions.add(add(buttonA = createButton(FlxG.width - 44 * 3, FlxG.height - 45 * 3, 44 * 3, 45 * 3, "a")));
 				actions.add(add(buttonB = createButton(FlxG.width - 86 * 3, FlxG.height - 45 * 3, 44 * 3, 45 * 3, "b")));
-				actions.add(add(buttonC = createButton(FlxG.width - (45 * 3), FlxG.height / 2 - 45 / 2, 44 * 3, 45 * 3, "c")));		
+                                actions.add(add(buttonC = createButton(FlxG.width - 44 * 3, FlxG.height - 85 * 3, 44 * 3, 45 * 3, "c")));	
 			case A_B_X_Y:
 				actions.add(add(buttonY = createButton(FlxG.width - 86 * 3, FlxG.height - 85 * 3, 44 * 3, 45 * 3, "y")));
 				actions.add(add(buttonX = createButton(FlxG.width - 44 * 3, FlxG.height - 85 * 3, 44 * 3, 45 * 3, "x")));					
