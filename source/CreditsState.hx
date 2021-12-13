988765949558aa4689967517add2edfdc088ba95
M.A. Jigsaw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-13 12:11:28 +0200

    For you

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -46,7 +46,7 @@ class CreditsState extends MusicBeatState
 		add(grpOptions);
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
-			['Android Port'],
+			['Psych Engine Android Port'],
 			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
