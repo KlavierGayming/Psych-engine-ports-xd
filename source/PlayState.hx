b040f555af32d173b2915fed6d011a7c7314943d
fang <95029829+Fangiolol@users.noreply.github.com>
2022-03-15 18:23:34 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1979,9 +1979,9 @@ class PlayState extends MusicBeatState
 		super.update(elapsed);
 
 		if(ratingString == '? (N/A)') {
-			scoreTxt.text = 'Pontos: ' + songScore + ' | Erros: ' + songMisses + ' | Avaliação: ' + ratingString;
+			scoreTxt.text = 'Pontos: ' + songScore + ' | Erros: ' + songMisses + ' | Precisão: ' + ratingString;
 		} else {
-			scoreTxt.text = 'Pontos: ' + songScore + ' | Erros: ' + songMisses + ' | Avaliação: ' + ratingString + ' ' + Math.floor(ratingPercent * 100) + '%';
+			scoreTxt.text = 'Pontos: ' + songScore + ' | Erros: ' + songMisses + ' | Precisão: ' + ratingString + ' ' + Math.floor(ratingPercent * 100) + '%';
 		}
 
 		if(cpuControlled) {
