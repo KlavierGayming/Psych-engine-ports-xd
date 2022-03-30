c1614413f3d379f43e288d41841fab1797b54011
fang <95029829+Fangiolol@users.noreply.github.com>
2022-03-30 19:33:16 -0300

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -8,6 +8,7 @@ import Controls;
 
 class ClientPrefs {
 	public static var downScroll:Bool = false;
+	public static var hitSounds:Bool = true;
 	public static var middleScroll:Bool = false;
 	public static var showFPS:Bool = true;
 	public static var flashing:Bool = true;
